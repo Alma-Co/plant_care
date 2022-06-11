ddd505543e13d42531e77cd11a941223c51b13e4
Alma-Co <edith.cantegrit@gmail.com>
2022-06-11 19:29:53 +0200

    create header and add sass, fontawesome

---
+++ plant_care/src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faMagnifyingGlass,
+  faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import App from "./App.vue";
+import router from "./router";
+
+/* add icons to the library */
+library.add(faCirclePlus, faMagnifyingGlass);
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.config.productionTip = false;
+app.mount("#app");
