c0d95567d36dd6bbae9240b52e64595eab769634
Alma-Co <edith.cantegrit@gmail.com>
2022-06-23 19:54:24 +0200

    upload icons pwa

--- my_plant_pwa/vue.config.js
+++ my_plant_pwa/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "Plantuary",
+    themeColor: "#EDE6DB",
+    msTileColor: "#417D7A",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#417D7A",
+  },
 });
