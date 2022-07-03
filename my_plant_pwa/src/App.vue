a68d9a988ba46d78baadfdbd01bde05bf660536a
Alma-Co <edith.cantegrit@gmail.com>
2022-07-03 19:19:01 +0200

    add footer component

--- my_plant_pwa/src/App.vue
+++ my_plant_pwa/src/App.vue
@@ -1,15 +1,33 @@
 <!-- Vue.Js -->
 <!--App.vue -->
+<script>
+import FooterNav from "./components/FooterNav.vue";
+
+export default {
+  components: {
+    FooterNav,
+  },
+  name: "App",
+};
+</script>
+
 <template>
   <!-- This serves all our pages using vue router -->
   <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" :key="$route.path" />
-    </transition>
+    <main class="main">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path" />
+      </transition>
+    </main>
+
+    <FooterNav />
   </router-view>
 </template>
 
 <style>
+.main {
+  padding: 0px 20px;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.4s;
@@ -19,8 +37,3 @@
   opacity: 0;
 }
 </style>
-<script>
-export default {
-  name: "App",
-};
-</script>
---
+++ my_plant_pwa/src/components/FooterNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <router-link :to="{ name: 'HelloWorld' }">
+      <h3>Home</h3>
+    </router-link>
+  </div>
+</template>
+
+<style>
+.container {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  padding: 20px;
+  background-color: red;
+  color: white;
+  z-index: 2;
+}
+</style>
+<script>
+export default {
+  name: "FooterNav",
+};
+</script>
