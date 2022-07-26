95fd4f504f66607dc181645eaad6e818cacea00c
Alma-Co <edith.cantegrit@gmail.com>
2022-07-26 19:49:50 +0200

    create list

--- my_plant_pwa/src/views/HelloWorld.vue
+++ my_plant_pwa/src/views/HelloWorld.vue
@@ -1,147 +1,67 @@
+<script>
+export default {
+  name: "HelloWorld",
+  setup() {
+    const plantsList = [
+      {
+        name: "plant 1",
+        image: "../assets/logo.png",
+        id: "1",
+      },
+      {
+        name: "plant 2",
+        image: "../assets/logo.png",
+        id: "2",
+      },
+      {
+        name: "plant 3",
+        image: "../assets/logo.png",
+        id: "3",
+      },
+      {
+        name: "plant 3",
+        image: "../assets/logo.png",
+        id: "3",
+      },
+    ];
+    return { plantsList };
+  },
+};
+</script>
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <font-awesome-icon icon="phone" />
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <div class="item-main">
-        <div class="item-wrapper-text">
-          <p>plant name</p>
-          <img src="../assets/logo.png" class="item-image" />
-        </div>
-      </div>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
+    <ul class="list-main">
+      <li class="item-main" v-for="plant in plantsList" :key="plant.id">
+        <img src="../assets/logo.png" class="item-image" />
+        <p class="item-text">plant name</p>
       </li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.item-main {
-  min-width: 200px;
+.list-main {
   display: flex;
   flex-direction: row;
+  list-style-type: none;
+  padding: 20px 0px;
 }
-.item-wrapper-text {
-  max-width: 180px;
-  height: 130px;
-  border-radius: 12px;
-  background-color: #ede6db;
+.item-main {
+  box-shadow: 0px 0px 18px rgb(0 0 0 / 8%);
+  border-radius: 10px;
+  margin: 0 10px;
+  background-color: white;
   color: black;
   padding: 10px;
 }
 
 .item-image {
-  height: 144px;
+  height: 20px;
   margin-bottom: 10px;
+}
+
+.item-text {
+  font-size: 14px;
 }
 </style>
