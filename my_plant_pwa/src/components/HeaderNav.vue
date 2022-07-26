e8dd62e3ad4f0e0c6a6f59acffe65aea2b0d26de
Alma-Co <edith.cantegrit@gmail.com>
2022-07-26 19:49:58 +0200

    style header

--- my_plant_pwa/src/components/HeaderNav.vue
+++ my_plant_pwa/src/components/HeaderNav.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="header-main">
+  <header class="header-main">
     <div class="header-wrapper">
-      <h3 class="header-title">My Plants</h3>
+      <h1 class="header-title">My Plants</h1>
       <div>
-        <font-awesome-icon
-          class="header-icon"
-          icon="fa-solid fa-magnifying-glass"
-        />
         <button class="header-button">
           <font-awesome-icon class="header-icon" icon="fa-solid fa-plus" />
         </button>
       </div>
     </div>
-  </div>
+  </header>
 </template>
 <style>
 .header-main {
+  /* font-family: "Indie Flower", cursive; */
+  font-family: "Poppins", sans-serif;
   position: fixed;
   top: 0;
   left: 0;
@@ -26,20 +24,34 @@
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 25px 20px;
-  background-color: white;
+  padding: 15px 25px 0px 25px;
+  background: linear-gradient(#f6f0f1, #00838f);
+  box-shadow: rgba(76, 72, 72, 0.31) 0px 2px 20px;
   z-index: 2;
 }
 .header-title {
-  color: #417d7a;
+  color: white;
+  font-size: 24px;
 }
 .header-button {
+  color: #00838f;
+  border-color: #00838f;
+  border-radius: 50px;
+  padding: 2px 4px;
   margin-left: 30px;
-  border-radius: 4px;
-  background-color: transparent;
 }
 .header-icon {
-  color: #417d7a;
+  color: #00838f;
+}
+
+@media only screen and (min-width: 768px) {
+  .header-main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 90%;
+    margin-left: 10%;
+  }
 }
 </style>
 <script>
