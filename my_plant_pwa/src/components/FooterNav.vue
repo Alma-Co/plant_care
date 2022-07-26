ef23d8bb27e8f0137b0f8fe849be2ad16952ab18
Alma-Co <edith.cantegrit@gmail.com>
2022-07-26 19:50:07 +0200

    style footer

--- my_plant_pwa/src/components/FooterNav.vue
+++ my_plant_pwa/src/components/FooterNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="footer-main">
+  <footer class="footer-main">
     <div class="footer-wrapper">
       <div v-for="entry in entriesMenu" :key="entry.title">
         <router-link class="footer-item" :to="{ name: 'HelloWorld' }">
@@ -8,10 +8,11 @@
         </router-link>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 <style>
 .footer-main {
+  font-family: "Indie Flower", cursive;
   position: fixed;
   bottom: 0;
   left: 0;
@@ -21,18 +22,43 @@
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 25px 20px;
-  background-color: white;
+  padding: 0px 25px;
+  background-color: #00838f;
   box-shadow: 0px -4px 14px rgba(0, 0, 0, 0.1);
   z-index: 2;
 }
 .footer-item {
-  color: #417d7a;
-  font-family: "DM Sans";
+  color: white;
   font-style: normal;
   text-decoration: none;
   text-underline-offset: none;
   text-transform: none;
+}
+
+@media only screen and (min-width: 768px) {
+  .footer-main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 10%;
+  }
+  .footer-wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 95%;
+    justify-content: space-between;
+    background-color: white;
+    box-shadow: 0px -4px 14px rgba(0, 0, 0, 0.1);
+    z-index: 2;
+  }
+  .footer-item {
+    color: #417d7a;
+    font-family: "DM Sans";
+    font-style: normal;
+    text-decoration: none;
+    text-underline-offset: none;
+    text-transform: none;
+  }
 }
 </style>
 <script>
